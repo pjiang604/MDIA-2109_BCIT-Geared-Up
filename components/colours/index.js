04ac246767a9604f0a00cc40730accf301920632
pjiang604 <patriciaejiang@gmail.com>
2023-02-26 21:20:46 -0800

    added map to colours component

--- components/colours/index.js
+++ components/colours/index.js
@@ -1,14 +1,32 @@
 import styles from '../colours/colours.module.css'
+import { useState, useEffect } from 'react'
+import { inventory } from '../../data/inventory'
 
 
 export default function Colours({
 
-    colour = "red"
+    colour = "red",
+
 
 }) {
+    const [data, setData] = useState([...inventory.clothing]);
+    const [color, setColor] = useState([...inventory.clothing])
+
+    const changeColor = (changeCol) => {
+        if (changeFil == "male") {
+            setFilter("Male");
+        } else if (changeFil == "female") {
+            setFilter("Female");
+        } else if (changeFil == "unisex") {
+            setFilter("Unisex");
+        }
+    }
+
     return (
-        <div className={styles.dots} style={{backgroundColor: colour}}>
-            {colour}
+        <div className={styles.dots} style={{ backgroundColor: colour }}>
+
+
+
         </div>
     )
 }
--- components/products/index.js
+++ components/products/index.js
@@ -1,24 +1,66 @@
 import Image from "next/image"
 import styles from '../products/product.module.css'
+import { inventory } from '../../data/inventory'
+import { useState } from 'react'
 
 
-export default function Product( {
+export default function Product({
 
     img = "",
     title = "",
-    colours = "",
+    colours = [],
+    hexCol = [],
     price = 0,
 
 }) {
+
+    const [circle, setCircle] = useState([inventory.clothing.hex]);
+
     return (
         <div className={styles.product}>
             <Image src={img} width="200" height="200" />
+
             <div>
                 <p>{title}</p>
             </div>
             <div>
                 <p>{price}</p>
             </div>
+            <div className={styles.colourDot}>
+                {circle && circle.map((info, index) => {
+                    {
+                        return (
+                            <div className={styles.colourDot} style={{ backgroundColor: `${colours}`}}>
+                                <span key={index}></span>
+                                {index}
+                                {colours}
+                                {hexCol}
+                            </div>
+                        )
+                    }
+                })}
+
+            </div>
         </div>
+
     )
 }
+
+/**            <div>
+                {colours.map(
+                    (colours, index) => {
+                        return (
+                            <div style={{ color: `${colours}` }}>
+                                <span style={{ backgroundColor: `${colours}` }} className={styles.colourDot}></span>
+                            </div>
+                        )
+                    }
+                )}
+            </div> 
+            
+            <div className={styles.colourDot} style={{backgroundColor: "red"}}>
+    <p>{colours}</p>
+</div>
+            
+            */
+
